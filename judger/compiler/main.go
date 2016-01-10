7f2ff6db95ea578417dae17c17246b2945b4aa3c
LackOfDream <galaxy0419@gmail.com>
2016-01-11 05:55:56 +0900

    bump

--- judger/compiler/main.go
+++ judger/compiler/main.go
@@ -3,9 +3,7 @@ package CorCppCompiler
 import (
 	"bytes"
 	"encoding/json"
-	"fmt"
 	"io/ioutil"
-	"lackofdream/oj/runInstanceModel"
 	"os"
 	"os/exec"
 	"time"
@@ -16,28 +14,19 @@ type CEMessage struct {
 	Message string
 }
 
-func delay(run runInstanceModel.Run) {
+func delay() {
 	time.Sleep(time.Second * 5)
-	infoOb := CEMessage{0, "timeout"}
-	info, _ := json.Marshal(infoOb)
-	ioutil.WriteFile(fmt.Sprintf("results/%d", run.Rid), info, 0644)
+	//	infoOb := CEMessage{0, "timeout"}
+	//	info, _ := json.Marshal(infoOb)
+	//	ioutil.WriteFile(fmt.Sprintf("results"), info, 0644)
 	os.Exit(1)
 }
 
-func Compile(run runInstanceModel.Run, srcFilePath, dstFilePath string) bool {
+func Compile(srcFilePath, dstFilePath string) bool {
 	var command *exec.Cmd
-	if run.Lang == "c++" {
-		command = exec.Command("g++", "-w", "-O2", "--static", "-lm", "-DONLINE_JUDGE", "-o", dstFilePath, srcFilePath)
-	} else if run.Lang == "c" {
-		command = exec.Command("gcc", "-w", "-O2", "--static", "-lm", "-DONLINE_JUDGE", "-o", dstFilePath, srcFilePath)
-	} else if run.Lang == "java" {
-		command = exec.Command("laaas")
-	} else {
-		command = exec.Command("laaas")
-	}
 	stderr, _ := command.StderrPipe()
 	command.Start()
-	go delay(run)
+	go delay()
 	defer command.Wait()
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(stderr)
@@ -45,7 +34,7 @@ func Compile(run runInstanceModel.Run, srcFilePath, dstFilePath string) bool {
 	if len(s) != 0 {
 		infoOb := CEMessage{0, s}
 		info, _ := json.Marshal(infoOb)
-		ioutil.WriteFile(fmt.Sprintf("results/%d", run.Rid), info, 0644)
+		ioutil.WriteFile("filename", info, 0644)
 		return false
 	}
 	return true
